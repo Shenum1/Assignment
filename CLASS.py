5a2ef6c9660e2eef37ea649972f75c08c880ad30
shenum1 <fanda.gabriel-istifanus@binghamuni.edu.ng>
2023-06-19 01:52:23 -0700

    todo1

---
+++ CLASS.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+def convert_fah_to_cel():
+    fahrenheit = float(ent_fah.get())
+    celsius =((5/9) * fahrenheit)-32
+    lbl_result['text'] = round(celsius, 2),"/N{DEGREE CELSIUS}"
+window = tk.Tk()
+window.title("fahrenheit to Celsius")
+
+frame = tk.Frame(master=window)
+ent_fah = tk.Entry(master=frame, width=10)
+lbl_fah = tk.Label(master=frame, text="\N{DEGREE FAHRENHEIT}")
+btn_convert = tk.Button(master=frame,command=convert_fah_to_cel, text= "Convert")
+lbl_result = tk.Label(master=frame, text="\N{DEGREE CELSIUS}")
+
+frame.grid(row=0, column=0)
+
+ent_fah.grid(row=0, column=0)
+ent_fah.grid(row=0, column=1)
+
+lbl_fah.grid(row=0, column=1)
+btn_convert.grid(row=0, column=2)
+lbl_result.grid(row=0, column=3)
+
+
+
+
+
+
+
+
+
+
+window.mainloop()
